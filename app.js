cee82ed5e23b488b0b46631e5f4881aaf5a219a1
likhe <likhelifi@laposte.net>
2016-12-11 21:33:46 +0100

    forgot app.js with no explination

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 "use strict";
-
-document.addEventListener("mousemove", function() {
-  console.log(arguments);
-});
+ function moves(e){
+  var cordx = 0;
+  var cordy = 0;
+  if (!e) {
+   var e = window.event;
+  }
+  if (e.pageX || e.pageY){
+   cordx = e.pageX;
+   cordy = e.pageY;
+  }
+  else if (e.clientX || e.clientY){
+   cordx = e.clientX;
+   cordy = e.clientY;
+  }
+  document.getElementById('box').style.left = cordx;
+  document.getElementById('box').style.top = cordy;
+ }
