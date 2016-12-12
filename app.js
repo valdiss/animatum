61724731ed9751b1b944a97703cc66ade036cf23
Khelifi Linda <lkhelifi@calade164.etu.ec-m.fr>
2016-12-12 13:46:09 +0100

    correct my code

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 "use strict";
 
-//faire bouger la div background sur une img fixe
+/*//faire bouger la div background sur une img fixe
 document.addEventListener("mousemove", function(ev) {
     //attribuer une variable let x = bouger horizontalement
       let x = ev.clientX;
@@ -14,4 +14,17 @@ document.addEventListener("mousemove", function(ev) {
     //si la souris bouge hor alors bouge horizontalement back
     //si souri bouge vert alors bouge verticalement back
 
-});
+});*/
+function getMouse(e) {
+  var posx;
+  var posy;
+  if (!e) var e = window.event;
+  if (e.pageX || e.pageY) {
+    posx = e.pageX;
+    posy = e.pageY;
+  }
+  else if (e.clientX || e.clientY) {
+    posx = e.clientX + document.body.scrollLeft + document.getElementByClass("scene").scrollLeft;
+    posy = e.clientY + document.body.scrollTop  + document.getElementByClass("scene").scrollTop;
+  }
+} //getMouse
