80b75e3b8c7ac967f8bd2844993afbc8413cf767
likhe <likhelifi@laposte.net>
2016-12-12 12:39:54 +0100

    check js modify html add in css

--- app.js
+++ app.js
@@ -3,9 +3,9 @@
 //faire bouger la div background sur une img fixe
 document.addEventListener("mousemove", function(ev) {
     //attribuer une variable let x = bouger horizontalement
-      let x = event.clientX;
+      let x = ev.clientX;
     //attribuer une variable let y = bouger verticalement
-      let y = event.clientY;
+      let y = ev.clientY;
 //Je prends ma div background
 //deb boucle for quand la souris bouge pas
   //si souris pas bouger alors pas bouger back
