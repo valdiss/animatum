2247381d7dccbeca8ec493698aed9290626ee0f5
LaBete <babaorhum001@gmail.com>
2016-12-14 15:51:13 +0100

    de rien

--- app.js
+++ app.js
@@ -1,41 +1,30 @@
+
 let mouseX;
 let mouseY;
-let img;
+let view = document.getElementById("view");
+let map = document.getElementById("map");
 let ratioW;
 let ratioH;
-let viewW;
-let viewH;
-let map;
-let view;
-
-let updateMouse = function(event) {
-    mouseX = event.clientX;
+let updateMouse = function()
+{
+    mouseX = (event.clientX) - (document.getElementById('mask_left').offsetWidth);
     mouseY = event.clientY;
-    console.log("e");
-
 }
-let scrollBackground = function(img, ratioW, ratioH) {
-    map.style["top"] = -mouseY * ratioH + "px";
-    map.style["left"] = -mouseX * ratioW + "px";
-
+let scrollBackground = function(map, ratioW, ratioH)
+{
+    map.style["top"] = -mouseY*ratioH + "px";
+    map.style["left"] = -mouseX*ratioW + "px";
 }
-
-let ready = function() {
-    view = document.getElementById('view');
-    viewW = view.offsetWidth;
-    viewH = view.offsetHeight;
-    map = document.getElementById('map');
-    mouseX = viewW / 2;
-    mouseY = viewH / 2;
-    ratioW = (map.offsetWidth - viewW) / viewW;
-    ratioH = (map.offsetHeight - viewH) / viewH;
-
-
-
+let ready = function()
+{
+    mouseX = view.offsetWidth/2;
+    mouseY = view.offsetHeight/2;
+    ratioW = (map.offsetWidth - view.offsetWidth)/view.offsetWidth;
+    ratioH = (map.offsetHeight - view.offsetHeight)/view.offsetHeight;
     view.addEventListener("mousemove", updateMouse);
-    view.addEventListener("mousemove", function() {
-        scrollBackground(img, ratioW, ratioH);
-    });
+    view.addEventListener("mousemove", function()
+            {
+                scrollBackground(map, ratioW, ratioH);
+            });
 }
-
 document.addEventListener("DOMContentLoaded", ready);
