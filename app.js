ab6dde60c54eff9cd3bdc2fa02257297164fd8b9
LaBete <babaorhum001@gmail.com>
2016-12-14 10:35:13 +0100

    fix bug scrolling background

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ let mouseY;
 let img;
 let ratioW;
 let ratioH;
+let viewW;
+let viewH;
+let map;
 
 let updateMouse = function()
 {
@@ -11,18 +14,22 @@ let updateMouse = function()
 }
 let scrollBackground = function(img, ratioW, ratioH)
 {
-    img.style["top"] = -mouseY*ratioH + "px";
-    img.style["left"] = -mouseX*ratioW + "px";
+  map.style.transform = "translateX("-mouseX*ratioW + "px)";
+  map.style.transform = "translateY("-mouseY*ratioH + "px)";
 }
 
 let ready = function()
 {
-    mouseX = window.innerWidth/2;
-    mouseY = window.innerHeight/2;
+    viewW = document.getElementById('view').offsetWidth;
+    console.log("hauteur de la div au centre" + viewW)
+    viewH = document.getElementById('view').offsetHeight;
+    console.log("la largeur de la div au centre" + viewH)
+    map = document.getElementById('map');
+    ratioW = (map.offsetWidth - viewW)/viewW;
+    ratioH = (map.offsetHeight - viewW)/viewW;
 
-    img = document.getElementById("D");
-    ratioW = (img.offsetWidth - window.innerWidth)/window.innerWidth;
-    ratioH = (img.offsetHeight - window.innerHeight)/window.innerHeight;
+    mouseX = viewW/2;
+    mouseY = viewH/2;
 
     document.addEventListener("mousemove", updateMouse);
     document.addEventListener("mousemove", function()
