58955bf274ae697cc88282eedda907d2d565bc0c
likhe <likhelifi@laposte.net>
2016-12-14 11:22:05 +0100

    upgrade files

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
-"use strict";
-let ready = function(){}
-//faire bouger la div background sur une img fixe
-//créer un évènement
-  let move = function(movementX, movementY) {
-    //quand la souris passe sur  la div scene
-      //si souris
+let mouseX;
+let mouseY;
+let img;
+let ratioW;
+let ratioH;
 
-    })
-};
-// DOM Bindings
-document.getElementById('scene').addEventListener("mousemove", move);
-document.addEventListener("DOMLoaded", ready);
+let updateMouse = function()
+{
+    mouseX = event.clientX;
+    mouseY = event.clientY;
+}
+let scrollBackground = function(img, ratioW, ratioH)
+{
+    img.style["top"] = -mouseY*ratioH + "px";
+    img.style["left"] = -mouseX*ratioW + "px";
+}
+
+let ready = function()
+{
+    mouseX = window.innerWidth/2;
+    mouseY = window.innerHeight/2;
+
+    img = document.getElementById("image");
+    ratioW = (img.offsetWidth - window.innerWidth)/window.innerWidth;
+    ratioH = (img.offsetHeight - window.innerHeight)/window.innerHeight;
+
+    document.addEventListener("mousemove", updateMouse);
+    document.addEventListener("mousemove", function()
+            {
+                scrollBackground(img, ratioW, ratioH);
+            });
+}
+document.addEventListener("DOMContentLoaded", ready);
