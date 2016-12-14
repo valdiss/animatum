ac89a1d7bead499e4807cb76570e7fead1b3c4ce
LaBete <babaorhum001@gmail.com>
2016-12-14 15:01:39 +0100

    commence a marcher

--- app.js
+++ app.js
@@ -6,35 +6,36 @@ let ratioH;
 let viewW;
 let viewH;
 let map;
+let view;
 
-let updateMouse = function()
-{
+let updateMouse = function(event) {
     mouseX = event.clientX;
     mouseY = event.clientY;
+    console.log("e");
+
 }
-let scrollBackground = function(img, ratioW, ratioH)
-{
-  map.style.transform = "translateX("-mouseX*ratioW + "px)";
-  map.style.transform = "translateY("-mouseY*ratioH + "px)";
+let scrollBackground = function(img, ratioW, ratioH) {
+    map.style["top"] = -mouseY * ratioH + "px";
+    map.style["left"] = -mouseX * ratioW + "px";
+
 }
 
-let ready = function()
-{
-    viewW = document.getElementById('view').offsetWidth;
-    console.log("hauteur de la div au centre" + viewW)
-    viewH = document.getElementById('view').offsetHeight;
-    console.log("la largeur de la div au centre" + viewH)
+let ready = function() {
+    view = document.getElementById('view');
+    viewW = view.offsetWidth;
+    viewH = view.offsetHeight;
     map = document.getElementById('map');
-    ratioW = (map.offsetWidth - viewW)/viewW;
-    ratioH = (map.offsetHeight - viewW)/viewW;
+    mouseX = viewW / 2;
+    mouseY = viewH / 2;
+    ratioW = (map.offsetWidth - viewW) / viewW;
+    ratioH = (map.offsetHeight - viewH) / viewH;
 
-    mouseX = viewW/2;
-    mouseY = viewH/2;
 
-    document.addEventListener("mousemove", updateMouse);
-    document.addEventListener("mousemove", function()
-            {
-                scrollBackground(img, ratioW, ratioH);
-            });
+
+    view.addEventListener("mousemove", updateMouse);
+    view.addEventListener("mousemove", function() {
+        scrollBackground(img, ratioW, ratioH);
+    });
 }
+
 document.addEventListener("DOMContentLoaded", ready);
