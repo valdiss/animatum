b8e0c429116e3f8f2d2f489e7783fa56432f101d
likhe <likhelifi@laposte.net>
2016-12-14 10:24:01 +0100

    add an animation

--- app.js
+++ app.js
@@ -1,30 +1,13 @@
 "use strict";
+let ready = function(){}
+//faire bouger la div background sur une img fixe
+//créer un évènement
+  let move = function(movementX, movementY) {
+    //quand la souris passe sur  la div scene
+      //si souris
 
-/*//faire bouger la div background sur une img fixe
-document.addEventListener("mousemove", function(ev) {
-    //attribuer une variable let x = bouger horizontalement
-      let x = ev.clientX;
-    //attribuer une variable let y = bouger verticalement
-      let y = ev.clientY;
-//Je prends ma div background
-//deb boucle for quand la souris bouge pas
-  //si souris pas bouger alors pas bouger back
-//Debut boucle For quand je bouge la souris bouge back
-  //Deb si
-    //si la souris bouge hor alors bouge horizontalement back
-    //si souri bouge vert alors bouge verticalement back
-
-});*/
-function getMouse(e) {
-  var posx;
-  var posy;
-  if (!e) var e = window.event;
-  if (e.pageX || e.pageY) {
-    posx = e.pageX;
-    posy = e.pageY;
-  }
-  else if (e.clientX || e.clientY) {
-    posx = e.clientX + document.body.scrollLeft + document.getElementByClass("scene").scrollLeft;
-    posy = e.clientY + document.body.scrollTop  + document.getElementByClass("scene").scrollTop;
-  }
-} //getMouse
+    })
+};
+// DOM Bindings
+document.getElementById('scene').addEventListener("mousemove", move);
+document.addEventListener("DOMLoaded", ready);
