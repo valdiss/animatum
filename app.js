211a94bd55e8b0ae08a38389eb2ae258b980c4c6
LaBete <babaorhum001@gmail.com>
2016-12-16 11:36:37 +0100

    le background fonctionne

--- app.js
+++ app.js
@@ -13,22 +13,23 @@ let updateMouse = function()
 {
     mouseX = (event.clientX) - (document.getElementById('mask_left').offsetWidth);
     mouseY = event.clientY;
+    console.log('a');
 }
 let scrollBackground = function(map, ratioW, ratioH)
 {
     map.style["top"] = -mouseY*ratioH + "px";
     map.style["left"] = -mouseX*ratioW + "px";
+    console.log('b');
 
-//    rabbit.style["top"] = -mouseY*ratioH + 700 +"px";
-//    rabbit.style["left"] = -mouseX*ratioW + 1250 + "px";
+    rabbit.style["top"] = -mouseY*ratioH + "px";
+    rabbit.style["left"] = -mouseX*ratioW + "px";
 
 }
 let ready = function()
 {
-
     mouseX = view.offsetWidth/2;
     mouseY = view.offsetHeight/2;
-    
+
     ratioW = (map.offsetWidth - view.offsetWidth)/view.offsetWidth;
     ratioH = (map.offsetHeight - view.offsetHeight)/view.offsetHeight;
 
@@ -38,9 +39,13 @@ let ready = function()
 
             {
                 scrollBackground(map, ratioW, ratioH);
+                console.log('d');
             });
     rabbit.addEventListener("click", function(){
         alert ('Bien joué gros!');
+        console.log('e');
     });
+    console.log('c');
+
 }
 document.addEventListener("DOMContentLoaded", ready);
