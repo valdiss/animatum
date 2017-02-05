bef08fbbded4f430f6f70880551fedd4695d678a
valdiss <valentin.dissais@gmail.com>
2017-02-05 11:43:47 +0100

    0.5s delay loading screen

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", function() {
-    document.getElementById('loadcover').style['display'] = "none";
+    document.getElementById('loadcover').classList.add('hidden');
 
     /*special Hotline Miami*/
     let hm = document.getElementById('hotline');
--- app_trankil.js
+++ app_trankil.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", function() {
-    document.getElementById('loadcover').style['display'] = "none";
+    document.getElementById('loadcover').classList.add('hidden');
 
     let mouseX;
     let mouseY;
